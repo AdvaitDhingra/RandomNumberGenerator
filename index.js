5d7e474c1faee7d8f14ed26e300f7cd37ffe461b
Advait73 <62119114+Advait73@users.noreply.github.com>
2020-12-14 14:13:55 +0100

    Added style

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function generate(){
     var d = new Date();
     var n = String(d.getTime());
-    var numberone = n.charAt(0);
-    var numbertwo = parseInt(n.charAt(12)) + 1
+    var numberone = parseInt(n.charAt(11)) + 1;
+    var numbertwo = parseInt(n.charAt(12)) + 1;
     var number = parseFloat("0" + "." + numbertwo)
     formula(parseFloat(number))
-    var a = formula(parseFloat(number));
+    var a = formula(numberone, parseFloat(number));
     console.log(a)
     var answer = a * 10
     console.log(answer)
     document.getElementById("number").innerHTML = answer;
 }
-function formula(x){
-    return((x + 3.8) * x * (1 - x));
+function formula(r, x){
+    return((r + 3.8) * x * (1 - x));
 }
